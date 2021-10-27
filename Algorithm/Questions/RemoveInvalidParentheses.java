c67c885ed1282564562ac797bf95845abb495514
WyXxyAi <17766540024@163.com>
2021-10-28 01:46:21 +0800

    October 28, 2021 01:46

---
+++ Algorithm/Questions/RemoveInvalidParentheses.java
@@ -0,0 +1,35 @@
+package Algorithm.Questions;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class RemoveInvalidParentheses {
+    public List<String> removeInvalidParentheses(String s) {
+        StringBuffer ss = new StringBuffer(s);
+        List<String> res = new ArrayList<>();
+        int left=0;
+        res.add(s);
+        for (int i = 0; i < ss.length(); i++) {
+            if (ss.charAt(i)=='('){
+                left+=1;
+            }else if (ss.charAt(i)==')'&&left>0){
+                left-=1;
+            }else if (ss.charAt(i)==')'&&left<=0){
+                ss.deleteCharAt(i);
+                i--;
+            }
+        }
+        res.add(ss.toString());
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "((aaaa))b)(a)))c)d";
+        List list= new RemoveInvalidParentheses().removeInvalidParentheses(s);
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
